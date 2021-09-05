c273a41ae36c3fb53b382eec8585d173afccc5fe
dupyo <42468263+dupyo@users.noreply.github.com>
2021-09-05 21:42:23 +0900

    demo -> week01

--- week01/src/test/java/com/sparta/week01/Week01ApplicationTests.java
+++ week01/src/test/java/com/sparta/week01/Week01ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.sparta.week01;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class Week01ApplicationTests {
 
 	@Test
 	void contextLoads() {
